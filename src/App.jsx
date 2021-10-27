79c00581d53b0c86e046691fcd002b90545d5748
elefteroff <elefteroff@mac.com>
2021-10-27 11:57:11 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div> 
+          <h1>Hello Dojo!</h1>
+          <h4>Things I need to do:</h4>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
